dfb0b09edc598e5982dfddd8a9a4c7c218289e0a
adrym1 <i202110771@cibertec.edu.pe>
2023-09-17 01:12:38 -0500

    creacion del proyecto

---
+++ src/main/java/pe/clinica/veterinariasanmarcos/VeterinariaSanMarcosApplication.java
@@ -0,0 +1,13 @@
+package pe.clinica.veterinariasanmarcos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaSanMarcosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeterinariaSanMarcosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/clinica/veterinariasanmarcos/VeterinariaSanMarcosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.clinica.veterinariasanmarcos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaSanMarcosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
